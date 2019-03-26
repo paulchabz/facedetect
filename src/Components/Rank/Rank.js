f8ceb81f5e88bbfabdeb03524c8d9b5e5b526a24
paulchabz <paulwchabz22@gmail.com>
2019-03-26 15:37:32 +0000

    Made changes to background of the app

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div className="white f3">
+      {`Paul, your current rank is ......`}
+      <div className="white f1">{`#5`}</div>
+    </div>
+  );
+};
+
+export default Rank;
